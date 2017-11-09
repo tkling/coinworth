3528828c7bad1274c5060280ea6c0409ff2d6871
tkling <tyling@gmail.com>
2017-11-09 10:33:03 -0500

    do favicon the express way, 2h cache time for stuff in public

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -14,13 +12,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { 'maxAge': '2h' }));
 
+var index = require('./routes/index');
 app.use('/', index);
 
 // catch 404 and forward to error handler
