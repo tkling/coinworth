abd5576bd453cdd17ada4e7183c23325eec60df7
tkling <tyling@gmail.com>
2017-11-09 15:42:01 -0500

    handle requests longer than 7 seconds as timedout

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var colors = require('colors');
+var timeout = require('connect-timeout');
 
 var app = express();
 
@@ -12,9 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
+// setup custom logger stuff
 logger.token('time', (req) => { return new Date().toTimeString().green; })
 logger.token('status', (req, res) => { return res.statusCode.toString().cyan })
 logger.token('method', (req, res) => { return req.method.toString().red })
@@ -35,6 +34,8 @@ logger.token('response-time', function getResponseTimeToken (req, res, digits) {
   return ms.toFixed(digits === undefined ? 3 : digits).toString().magenta
 })
 
+app.use(timeout('7s'))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger((tokens, req, res) => {
   return [
     tokens['time'](req, res),
@@ -48,8 +49,11 @@ app.use(logger((tokens, req, res) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(haltOnTimedout)
 app.use(cookieParser());
+app.use(haltOnTimedout)
 app.use(express.static(path.join(__dirname, 'public'), { 'maxAge': '2h' }));
+app.use(haltOnTimedout)
 
 var index = require('./routes/index');
 app.use('/', index);
@@ -71,5 +75,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function haltOnTimedout (req, res, next) {
+  if (!req.timedout) next()
+  else console.log('timed out!'.red)
+}
 
 module.exports = app;
